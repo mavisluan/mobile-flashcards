87c9be0f36fe2d932cd231529f864de1fbb631a8
Yi Angelov <mavisluan83@gmail.com>
2018-06-15 21:36:27 -0700

    create store

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, Text, Platform, StatusBar } from 'react-native';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+// import { createTabNavigator, createStackNavigator } from 'react-navigation'
+import reducers from './reducers'
+import { purple, white } from './utils/colors'
+import { Constants } from 'expo'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { setLocalNotification } from './utils/helpers'
+
 
 export default class App extends React.Component {
+  componentDidMount() {
+    setLocalNotification();
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View style={{ flex: 1 }} >
+          <View style={{ backgroundColor: purple, height: Constants.statusBarHeight }}>
+            <StatusBar translucent backgroundColor={purple} barStyle='light-content' />
+          </View>
+          <Text>MainNavigator</Text>
+        </View>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
