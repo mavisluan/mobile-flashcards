949cc1254189971cff105ae31d34c4b04eab30ef
Yi Angelov <mavisluan83@gmail.com>
2018-06-18 17:00:36 -0700

    Style Deck

--- components/Deck.js
+++ components/Deck.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import {
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  StyleSheet
 } from 'react-native'
 
 import { connect } from 'react-redux'
@@ -17,25 +18,31 @@ class Deck extends Component {
     const { navigation, deck } = this.props
 
     return (
-      <View>
-          <View>
-            <Text>{deck.title}</Text>
-            <Text>{deck.questions.length} cards</Text>
+      <View style={styles.container} >
+          <View style={styles.container}>
+            { deck.title === ''
+              ? <Text style={styles.titleCard}>No title</Text>
+              : <Text style={styles.titleCard}>{deck.title}</Text>
+            }
+            <Text style={{fontSize: 20}}>{deck.questions.length} cards</Text>
           </View>
-
+          <View style={styles.container}>
           <TouchableOpacity
+            style={styles.button}
             type='new'
             onPress={() => navigation.navigate(
               'NewCard',
               { deck: deck.title }
             )}
           >
-            <Text>Add card</Text>
+            <Text style={{fontSize: 20}}>Add card</Text>
           </TouchableOpacity>
 
 
         {
-          deck.questions.length !== 0 && <TouchableOpacity
+          deck.questions.length !== 0 &&
+          <TouchableOpacity
+            style={styles.button}
             type='quiz'
             onPress={() => {
               if (deck.questions.length !== 0) {
@@ -46,14 +53,41 @@ class Deck extends Component {
               }
             }}
           >
-              <Text>Quiz</Text>
+            <Text style={{fontSize: 20}}>Start Quiz</Text>
           </TouchableOpacity>
         }
-
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    alignItems:'center',
+  },
+  titleCard: {
+    color: Colors.blue,
+    fontSize: 32,
+    marginTop: 100
+  },
+  button: {
+    alignItems:'center',
+    backgroundColor: Colors.orange,
+    width: 200,
+    borderRadius : 16,
+    padding : 20,
+    marginBottom: 40,
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: Colors.gray,
+    shadowOffset: {
+      width: 0,
+      height: 3
+    }
+  },
+})
 
 function mapStateToProps ( decks, { navigation }) {
   const { title } = navigation.state.params
