4e3dfb8caa0bae46f00724a15037188714fd7328
Yi Angelov <mavisluan83@gmail.com>
2018-06-19 07:11:15 -0700

    Quiz component

---
+++ components/Quiz/result.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react'
+import styled from 'styled-components/native'
+import { AnimatedCircularProgress } from 'react-native-circular-progress'
+
+import { clearLocalNotification } from '../../utils/helpers'
+import * as Colors from '../../utils/colors'
+
+const Container = styled.View`
+  flex: 1
+  background-color: ${Colors.black}
+  justify-content: center
+  align-items: center
+`
+
+const Holder = styled.View`
+  margin-bottom: 40
+`
+
+const Button = styled.TouchableOpacity`
+  background-color: ${Colors.darkBlue}
+  margin-bottom: 10
+  height: 60
+  flex-direction: row
+  justify-content: center
+  align-items: center
+  align-self: stretch
+  margin-left: 20
+  margin-right: 20
+`
+
+const ButtonText = styled.Text`
+  font-size: 24
+  color: ${Colors.white}
+`
+
+const Text = styled.Text`
+  color: ${Colors.white}
+  font-size: 32
+  margin-bottom: 30
+  font-weight: 900
+`
+
+const Fill = styled.Text`
+  background-color: transparent
+  position: absolute
+  top: 72
+  left: 0
+  width: 200
+  text-align: center
+  color: ${Colors.white}
+  font-size: 50
+  font-weight: 900
+`
+
+export default class Result extends Component {
+  componentDidMount() {
+    clearLocalNotification()
+  }
+
+  render () {
+    const {correct, questions, onRetry} = this.props
+
+    return (
+      <Container>
+        {
+          (correct === questions) ?
+            <Text>Awesome!</Text> :
+            <Text>Almost there!</Text>
+        }
+
+        <Holder>
+          <AnimatedCircularProgress
+            size={200}
+            width={20}
+            fill={(correct/questions) * 100}
+            tintColor={Colors.green}
+            backgroundColor={Colors.yellow}
+            friction={10}>
+            {
+              (fill) => (
+                <Fill>
+                  { Math.round(fill) }
+                </Fill>
+              )
+            }
+          </AnimatedCircularProgress>
+        </Holder>
+
+        <Button
+          onPress={onRetry}
+        >
+          <ButtonText>Retry</ButtonText>
+        </Button>
+      </Container>
+    )
+  }
+}
