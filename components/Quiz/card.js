a46ff82b24b9491e1aba8f21bda067d99c9c87d6
Yi Angelov <mavisluan83@gmail.com>
2018-06-23 17:19:56 -0700

    update quiz card

--- components/Quiz/card.js
+++ components/Quiz/card.js
@@ -6,10 +6,64 @@ import {
   StyleSheet,
   Text,
   ScrollView,
+  Animated
 } from 'react-native'
 import FlipCard from 'react-native-flip-card'
-import Progress from './progress'
 import * as Colors from '../../utils/colors'
+
+class Progress extends Component {
+  componentWillMount() {
+    this.animation = new Animated.Value(this.props.progress)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.progress !== this.props.progress) {
+      Animated.timing(this.animation, {
+        toValue: this.props.progress,
+        duration: this.props.duration
+      }).start()
+    }
+  }
+
+  render() {
+    const {
+      height,
+      barColor,
+      fillColor
+    } = this.props
+
+    const widthInterpolated = this.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["0%", "100%"],
+      extrapolate: "clamp"
+    })
+
+    return (
+      <View style={{ height }}>
+        <View
+          style={[StyleSheet.absoluteFill, { backgroundColor: fillColor }]}
+        />
+        <Animated.View
+          style={{
+            position: "absolute",
+            left: 0,
+            top: 0,
+            bottom: 0,
+            width: widthInterpolated,
+            backgroundColor: barColor,
+          }}
+        />
+      </View>
+    )
+  }
+}
+
+Progress.defaultProps = {
+  height: 5,
+  barColor: Colors.gray,
+  fillColor: 'transparent',
+  duration: 100
+}
 
 
 export default ({onToggle, onCorrect, onIncorrect, step, questions, showAnswer}) => {
@@ -67,6 +121,9 @@ export default ({onToggle, onCorrect, onIncorrect, step, questions, showAnswer})
     </View>
   )
 }
+
+
+
 
 const styles = StyleSheet.create({
   flip: {
